6039c4507ebd8b676dda973785495df86023a3cd
val-is <vall_is@outlook.com>
2021-07-18 17:25:16 -0400

    Test new "compression" methods
    - subframes
    - line vectorization

--- converter/compress.py
+++ converter/compress.py
@@ -12,3 +12,29 @@ def compress_repeated(vals):
             prev = i
             count = 1
     return r
+
+
+# split a set of frames into ones that iterate through quadrants
+def split_quad_iter(frames):
+    q = 0
+    new = []
+    for frame in frames:
+        new.append(_get_quad_pixels(frame, q))
+        q = (q + 1) % 4
+    return new
+
+
+def _get_quad_pixels(frame, quad):
+    x_off, y_off = {
+            0: (1, 0),
+            1: (0, 0),
+            2: (0, 1),
+            3: (1, 1)
+            }[quad]
+    miniframe = []
+    for row in range(y_off, len(frame), 2):
+        r = []
+        for col in range(x_off, len(frame), 2):
+            r.append(frame[row][col])
+        miniframe.append(r)
+    return miniframe
--- converter/main.py
+++ converter/main.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import quadtree
 import compress
+import vec
 
 _INPUT = "../video/processing/badapple.mp4"    # needs to be gotten w/ yt-dl
 # _INPUT = "../video/processing/tmp.mp4"    # needs to be gotten w/ yt-dl
@@ -16,6 +17,7 @@ orig_framerate = video.get(cv2.CAP_PROP_FPS)
 frame_skip = orig_framerate // _TARGET_FPS
 
 frames = [] # yeah, we load it into mem. so what lul
+frames_raw = []
 
 cur_frame = frames_written = 0
 prev_frame = None
@@ -34,16 +36,23 @@ while video.isOpened():
                 interpolation=cv2.INTER_NEAREST_EXACT)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         _, frame = cv2.threshold(frame, 127, 255, cv2.THRESH_BINARY)
+        frame_raw = cv2.bitwise_and(frame, frame)
+        frame_raw = frame_raw.tolist()
         xored_frame = cv2.bitwise_xor(frame, prev_frame)
         xored_frame = xored_frame.tolist()
-        for i in range(len(xored_frame)):
-            for j in range(len(xored_frame[i])):
-                if xored_frame[i][j] != 0:
-                    xored_frame[i][j] = 1
-                else:
-                    xored_frame[i][j] = 0
+        def _conv_frame(frame):
+            for i in range(len(frame)):
+                for j in range(len(frame[i])):
+                    if frame[i][j] != 0:
+                        frame[i][j] = 1
+                    else:
+                        frame[i][j] = 0
+        _conv_frame(xored_frame)
         frames.append(xored_frame)
+        _conv_frame(frame_raw)
+        frames_raw.append(frame_raw)
     cur_frame += 1
+    prev_frame = cur_frame
 video.release()
 
 def copy_frame(frame):
@@ -74,12 +83,47 @@ def quadtree_compress():
     return quadtree_compressed
 
 pos_compress = quadtree_compress()
-print(len(pos_compress))
+print(f"quadtree positive: {len(pos_compress)}")
 pos_compress_repeated = compress.compress_repeated(pos_compress)
-print(len(pos_compress_repeated))
+print(f"quadtree positive repeated: {len(pos_compress_repeated)}")
 
 quadtree.set_positive_frame(0)
 neg_compress = quadtree_compress()
-print(len(neg_compress))
+print(f"quadtree negative: {len(neg_compress)}")
 neg_compress_repeated = compress.compress_repeated(neg_compress)
-print(len(neg_compress_repeated))
+print(f"quadtree negative repeated: {len(neg_compress_repeated)}")
+
+
+def compress_lines(frames):
+    bitstream = []
+    for f in frames:
+        lines = vec.get_frame_lines(f)
+        for line in lines:
+            for val in line:
+                bitstream.append(val)
+            assert sum(line) == len(frames[0][0])
+    return bitstream
+
+line_compress = compress_lines(frames_raw)
+print(f"line: {len(line_compress)}")
+line_compress_repeated = compress.compress_repeated(line_compress)
+print(f"line repeated: {len(line_compress_repeated)}")
+
+line_compress_xor = compress_lines(frames)
+print(f"xor line: {len(line_compress_xor)}")
+line_compress_repeated_xor = compress.compress_repeated(line_compress_xor)
+print(f"xor line repeated: {len(line_compress_repeated_xor)}")
+
+
+quad_frames_raw = compress.split_quad_iter(frames_raw)
+quad_frames_xor = compress.split_quad_iter(frames)
+
+line_compress = compress_lines(quad_frames_raw)
+print(f"quad line: {len(line_compress)}")
+line_compress_repeated = compress.compress_repeated(line_compress)
+print(f"quad line repeated: {len(line_compress_repeated)}")
+
+line_compress_xor = compress_lines(quad_frames_xor)
+print(f"quad xor line: {len(line_compress_xor)}")
+line_compress_repeated_xor = compress.compress_repeated(line_compress_xor)
+print(f"quad xor line repeated: {len(line_compress_repeated_xor)}")
